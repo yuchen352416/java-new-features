bc632de9da5074f01ae80c587ff8bbcd8299a17e
Selfimpr <yuchen352416@163.com>
2019-05-02 22:24:06 +0800

    修改 gitignore

--- src/main/java/pro/yuchen/demo/java8/chapter_02/Example001.java
+++ src/main/java/pro/yuchen/demo/java8/chapter_02/Example001.java
@@ -1,5 +1,9 @@
 package pro.yuchen.demo.java8.chapter_02;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @Description:
  * @Author: Mr.Lee
@@ -8,9 +12,9 @@ package pro.yuchen.demo.java8.chapter_02;
 public class Example001 {
 
 	public static void main(String[] args) {
-
-
-
+		Stream<String> stream = Stream.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+		List<String> list = stream.limit(4).collect(Collectors.toList());
+		list.forEach(System.out::println);
 	}
 
 
