fc5969305c62ab6e9bfb15f9fedb03e92054f7ed
Selfimpr <yuchen352416@163.com>
2019-05-02 21:59:19 +0800

    初始化项目

---
+++ src/main/java/pro/yuchen/demo/java8/chapter_02/Example001.java
@@ -0,0 +1,17 @@
+package pro.yuchen.demo.java8.chapter_02;
+
+/**
+ * @Description:
+ * @Author: Mr.Lee
+ * @Date: 2019-05-02 21:58
+ */
+public class Example001 {
+
+	public static void main(String[] args) {
+
+
+
+	}
+
+
+}
