28a3f146c06966d9537c50676f4110e57a156331
Selfimpr <yuchen352416@163.com>
2019-05-25 15:00:29 +0800

    添加PDF处理Demo

--- src/main/java/pro/yuchen/demo/java8/chapter/Demo_002.java
+++ src/main/java/pro/yuchen/demo/java8/chapter/Demo_002.java
@@ -1,13 +1,9 @@
 package pro.yuchen.demo.java8.chapter;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
 
 import java.io.FileOutputStream;
-import java.util.List;
 
 /**
  * @description:
@@ -19,7 +15,7 @@ public class Demo_002 {
 	public static void main(String[] args) throws Exception {
 
 		String pdfPath = "/Users/Selfimpr/Desktop/letter.pdf";
-		String imagePath = "/Users/Selfimpr/Desktop/confirmation.jpg";
+		String imagePath = "/Users/Selfimpr/Desktop/zzz.png";
 		String outPath = "/Users/Selfimpr/Desktop/test.pdf";
 
 		/*
@@ -32,14 +28,65 @@ public class Demo_002 {
 		pdf.close();
 		*/
 
-		Document document = new Document(PageSize.A4, 36, 36, 36, 36);
-		PdfWriter.getInstance(document, new FileOutputStream(pdfPath));
-		document.open();
-		
+//		Document document = new Document(PageSize.A4, 0, 0, 0, 0);
+//		PdfWriter.getInstance(document, new FileOutputStream(pdfPath));
+//		document.open();
+//		BGPic border = new BGPic();
+//		float wid = 80f;
+//		float hei = 100f;
+//		float[] widArr = { wid, wid };
+//		PdfPTable table = new PdfPTable(2);
+//		table.setTotalWidth(widArr);
+//		table.setLockedWidth(true);
+//		PdfPCell cell = null;
+//
+//		for (int i = 1; i <= 4; i++) {
+//			Image img = Image.getInstance(imagePath);
+//			cell = new PdfPCell(img, true);
+//			cell.setFixedHeight(hei);
+//			cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//			cell.setPadding(8);
+//			cell.setCellEvent(border);  // 加入背景图片
+//			table.addCell(cell);
+//		}
+//
+//		document.add(table);
+//		document.close();
 
 
 
+		PdfReader reader = new PdfReader(pdfPath);
+		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(outPath));
+		PdfContentByte content = stamper.getOverContent(1);
+		Image image = Image.getInstance(imagePath);
+		image.setAbsolutePosition(0f, 0f);
+		image.scaleAbsolute(reader.getCropBox(1).getWidth(), reader.getCropBox(1).getHeight());
+		image.setAlignment(Image.ALIGN_CENTER);
+//		layer
+
+		content.addImage(image);
+		stamper.close();
+		reader.close();
 
 	}
 
+
+}
+
+
+class BGPic implements PdfPCellEvent {
+	public void cellLayout(PdfPCell cell, Rectangle rect, PdfContentByte[] canvas) {
+		PdfContentByte cb = canvas[PdfPTable.BACKGROUNDCANVAS];
+		Image img = null;
+		try {
+			img = Image.getInstance("/Users/Selfimpr/Desktop/confirmation.jpg");
+			img.scaleAbsolute(80, 100);  // 设置背景图片的大小
+			img.setAbsolutePosition(298, 606);  // 设置第一个背景图片的绝对位置
+			cb.addImage(img);
+			img.setAbsolutePosition(217, 706);  // 设置第二个背景图片的绝对位置
+			cb.addImage(img);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
