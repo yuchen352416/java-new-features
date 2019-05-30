683241a3cf07840ea49b29bcec9f248bdf5489ca
Selfimpr <yuchen352416@163.com>
2019-05-30 18:39:06 +0800

    添加多线程测试类

---
+++ src/main/java/pro/yuchen/demo/java8/chapter_03/ExecutorDemo.java
@@ -0,0 +1,14 @@
+package pro.yuchen.demo.java8.chapter_03;
+
+/**
+ * @description:
+ * @author: Mr.Lee
+ * @date: 2019-05-30 18:16
+ */
+public class ExecutorDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
